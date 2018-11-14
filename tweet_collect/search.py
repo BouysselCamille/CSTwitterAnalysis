e20248ebc0ca35a5d2a1c03949390680cb0f5b9f
Your Name <you@example.com>
2018-11-14 14:01:48 +0100

    fonctionnalité 2

---
+++ tweet_collect/search.py
@@ -0,0 +1,45 @@
+import tweet_collect.twitter_connection_setup as connect
+
+
+def collect():
+    connexion = connect.twitter_setup()
+    tweets = connexion.search("@EmmanuelMacron",language="french",rpp=100)
+    for tweet in tweets:
+        print(tweet.text)
+
+
+def collect_by_user(user_id):
+    connexion = connect.twitter_setup()
+    statuses = connexion.user_timeline(id = user_id, count = 200)
+    for status in statuses:
+        print(status.text)
+    return statuses
+#collect_by_user(25073877)
+
+import tweepy
+from tweepy.streaming import StreamListener
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        print(data)
+        return True
+
+    def on_error(self, status):
+        if  str(status) == "420":
+            print(status)
+            print("You exceed a limited number of attempts to connect to the streaming API")
+            return False
+        else:
+            return True
+
+
+
+
+def collect_by_streaming():
+
+    connexion = connect.twitter_setup()
+    listener = StdOutListener()
+    stream=tweepy.Stream(auth = connexion.auth, listener=listener)
+    stream.filter(track=['Emmanuel Macron'])
+
+collect_by_streaming()
