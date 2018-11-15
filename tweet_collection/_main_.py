da49836d3cb74c4103b87dcdc7cd37a310ffd8d2
Your Name <you@example.com>
2018-11-15 11:43:16 +0100

    fonctionnalité 5

--- tweet_collection/_main_.py
+++ tweet_collection/_main_.py
@@ -7,16 +7,23 @@ connexion=connect.twitter_setup()
 
 from tweet_collection.collect_candidate_actuality_tweets import get_candidate_queries,get_tweets_from_candidates_search_queries
 def store_tweets(tweets,filename):
-    for tweet in tweets[1]:
-        print(tweet)
+    os.chdir('/Users/camille/PycharmProjects/twitterPredictor/tweet_collection')
+    for tweet in tweets:
         with open(filename, "w") as write_file:
-            json.dump(tweet, write_file)
+            json.dump(tweet._json, write_file)
     return()
 
 queries_1976143068=get_candidate_queries(1976143068,'/Users/camille/PycharmProjects/twitterPredictor/CandidateData')
 
 tweets_to_store=get_tweets_from_candidates_search_queries(queries_1976143068, connexion)
-print(tweets_to_store)
+#print(tweets_to_store)
 store_tweets(tweets_to_store,"tweets.json")
 
+from tweet_collect.search import collect
+from pytest import *
 
+
+def test_collect():
+    tweets = collect()
+    data =  transform_to_dataframe(tweets)
+    assert 'tweet_textual_content' in data.columns
