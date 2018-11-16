d38c47de3d663fbdaf18a74f72081ca202719923
Your Name <you@example.com>
2018-11-16 10:27:04 +0100

    fonctionnalité 6

--- tweet_analysis/analysis.py
+++ tweet_analysis/analysis.py
@@ -6,9 +6,9 @@ import pandas as pd
 import numpy as np
 connexion=connect.twitter_setup()
 
-def collect_to_pandas_dataframe():
+def collect_to_pandas_dataframe(querry):
     connexion = connect.twitter_setup()
-    tweets = connexion.search("@EmmanuelMacron",language="fr",rpp=100)
+    tweets = connexion.search(querry,language="fr",rpp=100)
     data = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['tweet_textual_content'])
     data['len']  = np.array([len(tweet.text) for tweet in tweets])
     data['ID']   = np.array([tweet.id for tweet in tweets])
@@ -18,37 +18,49 @@ def collect_to_pandas_dataframe():
     data['RTs']    = np.array([tweet.retweet_count for tweet in tweets])
     return data
 
-data=collect_to_pandas_dataframe()
-
-print(data)
+data_trump=collect_to_pandas_dataframe("@realDonaldTrump")
+data_emmanuel=collect_to_pandas_dataframe("@EmmanuelMacron")
+print(data_trump)
 
 
 # Max RTs:
-rt_max  = np.max(data['RTs'])
-rt  = data[data.RTs == rt_max].index[0]
-print("The tweet with more retweets is: \n{}".format(data['tweet_textual_content'][rt]))
+rt_max  = np.max(data_emmanuel['RTs'])
+rt  = data_emmanuel[data_emmanuel.RTs == rt_max].index[0]
+print("The tweet with more retweets is: \n{}".format(data_emmanuel['tweet_textual_content'][rt]))
 print("Number of retweets: {}".format(rt_max))
-print("{} characters.\n".format(data['len'][rt]))
+print("{} characters.\n".format(data_emmanuel['len'][rt]))
 
 
 #le plus récent
-plus_recent=np.max(data['Date'])
-rt=data[data.Date == plus_recent].index[0]
-print("The most recent tweet is : \n{}".format(data['tweet_textual_content'][rt]))
+plus_recent=np.max(data_emmanuel['Date'])
+rt=data_emmanuel[data_emmanuel.Date == plus_recent].index[0]
+print("The most recent tweet is : \n{}".format(data_emmanuel['tweet_textual_content'][rt]))
 print("Published {}.\n".format(plus_recent))
 
 #le plus liké
-plus_like=np.max(data['Likes'])
-rt=data[data.Likes == plus_like].index[0]
-print("The most liked tweet is : \n{}".format(data['tweet_textual_content'][rt]))
+plus_like=np.max(data_emmanuel['Likes'])
+rt=data_emmanuel[data_emmanuel.Likes == plus_like].index[0]
+print("The most liked tweet is : \n{}".format(data_emmanuel['tweet_textual_content'][rt]))
 print("Number of likes: {}.\n".format(plus_like))
 
+
+#####visualisation####
 import matplotlib.pyplot as plt
-tfav = pd.Series(data=data['Likes'].values, index=data['Date'])
-tret = pd.Series(data=data['RTs'].values, index=data['Date'])
+tfav = pd.Series(data=data_emmanuel['Likes'].values, index=data_emmanuel['Date'])
+tret = pd.Series(data=data_emmanuel['RTs'].values, index=data_emmanuel['Date'])
 
 # Likes vs retweets visualization:
 tfav.plot(figsize=(16,4), label="Likes", legend=True)
 tret.plot(figsize=(16,4), label="Retweets", legend=True)
+
+plt.show()
+plt.close()
+
+#Trump vs Macron
+tret_macron=pd.Series(data=data_emmanuel['RTs'].values, index=data_emmanuel['Date'])
+tret_trump=pd.Series(data=data_trump['RTs'].values, index=data_trump['Date'])
+
+tret_macron.plot(figsize=(16,4), label="Macron", legend=True)
+tret_trump.plot(figsize=(16,4), label="Trump", legend=True)
 
 plt.show()
