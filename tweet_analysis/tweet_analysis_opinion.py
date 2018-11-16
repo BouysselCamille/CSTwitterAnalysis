6a90ac62edb13b0c24006c018093ddc828c25e5d
Clara Ni <clara.ni@student-cs.fr>
2018-11-16 16:57:03 +0100

    Coooooool

---
+++ tweet_analysis/tweet_analysis_opinion.py
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#print("Percentage of positive tweets: {}%".format(len(pos_tweets)*100/len(data['tweet_textual_content'])))
+#print("Percentage of neutral tweets: {}%".format(len(neu_tweets)*100/len(data['tweet_textual_content'])))
+#print("Percentage de negative tweets: {}%".format(len(neg_tweets)*100/len(data['tweet_textual_content'])))
