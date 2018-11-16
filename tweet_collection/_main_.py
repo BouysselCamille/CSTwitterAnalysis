d107cb348dff5c752c3a04b80c48f0224d443828
Your Name <you@example.com>
2018-11-16 09:18:46 +0100

    fonctionnalité 5

--- tweet_collection/_main_.py
+++ tweet_collection/_main_.py
@@ -2,6 +2,8 @@ import os
 import tweepy
 import json
 import tweet_collect.twitter_connection_setup as connect
+import pandas as pd
+import numpy as np
 connexion=connect.twitter_setup()
 
 
@@ -23,7 +25,26 @@ from tweet_collect.search import collect
 from pytest import *
 
 
-def test_collect():
-    tweets = collect()
-    data =  transform_to_dataframe(tweets)
-    assert 'tweet_textual_content' in data.columns
+
+
+def collect_to_pandas_dataframe():
+    connexion = connect.twitter_setup()
+    tweets = connexion.search("@EmmanuelMacron",language="fr",rpp=100)
+    data = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['tweet_textual_content'])
+    data['len']  = np.array([len(tweet.text) for tweet in tweets])
+    data['ID']   = np.array([tweet.id for tweet in tweets])
+    data['Date'] = np.array([tweet.created_at for tweet in tweets])
+    data['Source'] = np.array([tweet.source for tweet in tweets])
+    data['Likes']  = np.array([tweet.favorite_count for tweet in tweets])
+    data['RTs']    = np.array([tweet.retweet_count for tweet in tweets])
+    return data
+
+data=collect_to_pandas_dataframe()
+
+rt_max  = np.max(data['RTs'])
+rt  = data[data.RTs == rt_max].index[0]
+
+# Max RTs:
+print("The tweet with more retweets is: \n{}".format(data['tweet_textual_content'][rt]))
+print("Number of retweets: {}".format(rt_max))
+print("{} characters.\n".format(data['len'][rt]))
